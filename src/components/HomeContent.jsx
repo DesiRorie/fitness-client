e68a6a5da28da8625f9c27f400585c99f3131703
DesiRorie <desirorie@icloud.com>
2023-05-31 19:23:49 -0400

    .

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import DirectionsRunIcon from "@mui/icons-material/DirectionsRun";
 import { useSelector } from "react-redux";
 import SetGoals from "../pages/SetGoals";
+
 useSelector;
 
 const HomeContent = () => {
@@ -15,48 +16,47 @@ const HomeContent = () => {
         <h2>{username}'s goal for today</h2>
         <button>Edit</button>
       </div>
-      <div className="calorieDiv">
-        <h3>Calories</h3>
-        <p style={{ color: "#888" }}>Remaining = Goal - Food + Exercise</p>
-        <div className="calorieSplit">
-          <div>
-            <span>{goal} </span>
-            <span>remaining</span>
-          </div>
-          <div>
-            <ul>
-              <li>
-                Base Goal <span>{goal}</span>
-              </li>
-              <li></li>
-            </ul>
+      <div className="calorieParent">
+        <div className="calorieDiv">
+          <div className="calorieChildren">
+            <h3>Calories</h3>
+            <p style={{ color: "#888" }}>Remaining = Goal - Food + Exercise</p>
+            <div className="calorieSplit">
+              <div>
+                <span>{goal} </span>
+                <span>remaining</span>
+              </div>
+              <div>
+                <ul>
+                  <li>
+                    Base Goal <span>{goal}</span>
+                  </li>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
       <div className="stepsAndExercise">
-        <div className="stepsChild space">
+        <div className="stepsChild ">
           <h4>Steps</h4>
           <div className="stepsDiv">
             <DirectionsRunIcon />
             <p>Conect to track your steps</p>
           </div>
         </div>
-        <div className="exerciseChild space">
-          <div className="exerciseDiv">
-            <h4>Exercise</h4>
-            <span>+</span>
-          </div>
-          <span> insert calories burned from exercise</span>
-          <span> insert time exercise</span>
-        </div>
       </div>
       <div className="weightChart">
-        <div className="weightChild">
-          <h4>Weight </h4>
-          <span>+</span>
-        </div>
+        <div className="calorieChildren">
+          <div className="weightChild">
+            <h4>Weight </h4>
+            <span>+</span>
+          </div>
 
-        <span>Last 90 days</span>
+          <span>Last 90 days</span>
+        </div>
       </div>
     </div>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -21,30 +21,32 @@ const LoginPage = () => {
   const dispatch = useDispatch();
   return (
     <div className="loginDiv">
-      <h1>Login</h1>
-      <form>
-        <input
-          onKeyPress={handleKeyPress}
-          onChange={(e) => {
-            setNewUsername(e.target.value);
-          }}
-          type="text"
-          placeholder="username"
-        />
-        <button
-          type="button"
-          disabled={isButtonDisabled}
-          onClick={() => {
-            dispatch(
-              login({
-                username: newUsername,
-              })
-            );
-          }}
-        >
-          Login
-        </button>
-      </form>
+      <div className="loginBox">
+        <h1>Login</h1>
+        <form>
+          <input
+            onKeyPress={handleKeyPress}
+            onChange={(e) => {
+              setNewUsername(e.target.value);
+            }}
+            type="text"
+            placeholder="username"
+          />
+          <button
+            type="button"
+            disabled={isButtonDisabled}
+            onClick={() => {
+              dispatch(
+                login({
+                  username: newUsername,
+                })
+              );
+            }}
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
